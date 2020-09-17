9c32c268f60b7f1c461e987a47dd09afbd363518
aldis19-fjall <aldis19@ru.is>
2020-09-17 11:22:53 +0000

    hello

--- tiletraveller.py
+++ tiletraveller.py
@@ -1,1 +1,14 @@
 # verkefni2
+
+def func()
+position = 1.1 
+ir_str = input('Direction: ') 
+if ir_str == 'n': 
+    position += 1 
+elif ir_str == 's': 
+    position += -1 
+elif ir_str == 'a': 
+    position += 0.1 
+elif ir_str == 'v': 
+    position += -0.1 
+print('You can travel:', ir_str) 
