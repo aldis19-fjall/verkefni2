2460f7ab5424d2afe5c4d5d7ca3478ae168b41df
AlexArnes20 <alexa20@ru.is>
2020-09-17 11:08:36 +0000

    hallo

--- tiletraveller.py
+++ tiletraveller.py
@@ -1,31 +1,62 @@
 # verkefni2
 
-
-
-position = 1.1
-
-def func(north):
+def func(ir_str):
     
 
 
+    position = 1.1
+    pos_dir = ''
 
 
+    ir_str =  input('Direction: ')
 
 
+    
+def movement(position, ir_str):
 
-ir_str =  input('Direction: ')
+    if ir_str == 'n' or ir_str == 'N':
+        position += 1
 
-if ir_str == 'n':
-    position += 1
+    if ir_str == 's' or ir_str == 'S':
+        position += -1
 
-elif ir_str == 's':
-    position += -1
+    if ir_str == 'a' or ir_str == 'A':
+        position += 0.1
 
-elif ir_str == 'a':
-    position += 0.1
-
-elif ir_str == 'v':
-    position += -0.1
+    if ir_str == 'v' or ir_str == 'V':
+        position += -0.1
+    return position
 
 
-print('You can travel:', ) 
+def positioning(position):
+    if position == 1.1:
+        pos_dir = '(N)orth or (E)ast.'
+
+    if position == 1.2:
+        pos_dir = '(N)orth or (E)ast or (S)outh.'
+
+    if position == 1.3:
+        pos_dir = '(S)outh or (E)ast.'
+
+    if position == 2.1:
+        pos_dir = '(N)orth or (E)ast or (W)est.'
+
+    if position == 2.2:
+        pos_dir = '(N)orth or (E)ast.'
+
+    if position == 2.3:
+        pos_dir = '(N)orth or (E)ast.'
+
+    if position == 3.1:
+        pos_dir = '(N)orth or (E)ast.'
+
+    if position == 3.2:
+        pos_dir = '(N)orth or (E)ast.'
+
+    if position == 3.3:
+        pos_dir = '(N)orth or (E)ast.'
+    return pos_dir
+
+
+    print('You can travel:', ) 
+
