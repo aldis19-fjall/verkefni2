7d2e729a9267d7a82924b17278dd06989e10c282
AlexArnes20 <alexa20@ru.is>
2020-09-17 10:24:46 +0000

    hallo

--- tiletraveller.py
+++ tiletraveller.py
@@ -1,1 +1,31 @@
 # verkefni2
+
+
+
+position = 1.1
+
+def func(north):
+    
+
+
+
+
+
+
+
+ir_str =  input('Direction: ')
+
+if ir_str == 'n':
+    position += 1
+
+elif ir_str == 's':
+    position += -1
+
+elif ir_str == 'a':
+    position += 0.1
+
+elif ir_str == 'v':
+    position += -0.1
+
+
+print('You can travel:', ) 
