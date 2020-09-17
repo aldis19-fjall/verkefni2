6c6aef0623a5f513c8bca8081d765557e3911e82
AlexArnes20 <alexa20@ru.is>
2020-09-17 11:49:57 +0000

    hallo

--- tiletraveller.py
+++ tiletraveller.py
@@ -1,14 +1,12 @@
 # verkefni2
 
-def func(ir_str):
-    
 
 
-    position = 1.1
-    pos_dir = ''
+position = 1.1
+pos_dir = ''
 
 
-    ir_str =  input('Direction: ')
+ir_str =  input('Direction: ')
 
 
     
@@ -30,32 +28,52 @@ def movement(position, ir_str):
 
 def positioning(position):
     if position == 1.1:
-        pos_dir = '(N)orth or (E)ast.'
+        pos_dir = '(N)orth.'
+        if ir_str != 'n' or ir_str != 'N':
+            print('Not a valid direction!')
 
     if position == 1.2:
         pos_dir = '(N)orth or (E)ast or (S)outh.'
+        if ir_str == 'w' or ir_str == 'W':
+            print('Not a valid direction!')
 
     if position == 1.3:
         pos_dir = '(S)outh or (E)ast.'
+        if ir_str == 'w' or ir_str == 'n' or ir_str == 'W' or ir_str == 'N':
+            print('Not a valid direction!')
 
     if position == 2.1:
-        pos_dir = '(N)orth or (E)ast or (W)est.'
+        pos_dir = '(N)orth.'
+        if ir_str != 'n' or ir_str != 'N':
+            print('Not a valid direction!')
 
     if position == 2.2:
-        pos_dir = '(N)orth or (E)ast.'
+        pos_dir = '(W)est or (S)outh.'
+        if ir_str == 'e' or ir_str == 'n' or ir_str == 'E' or ir_str == 'N':
+            print('Not a valid direction!')
 
     if position == 2.3:
-        pos_dir = '(N)orth or (E)ast.'
-
-    if position == 3.1:
-        pos_dir = '(N)orth or (E)ast.'
-
-    if position == 3.2:
-        pos_dir = '(N)orth or (E)ast.'
+        pos_dir = '(W)est or (E)ast.'
+        if ir_str == 's' or ir_str == 'n' or ir_str == 'S' or ir_str == 'N':
+            print('Not a valid direction!')
 
     if position == 3.3:
-        pos_dir = '(N)orth or (E)ast.'
+        pos_dir = '(W)est or (S)outh.'
+        if ir_str == 'e' or ir_str == 'n' or ir_str == 'E' or ir_str == 'N':
+            print('Not a valid direction!')
+
+    if position == 3.2:
+        pos_dir = '(N)orth or (S)outh.'
+        if ir_str == 'e' or ir_str == 'w' or ir_str == 'E' or ir_str == 'W':
+            print('Not a valid direction!')
+
+    if position == 3.1:
+        pos_dir = 'Victory!'
+
     return pos_dir
+
+
+
 
 
     print('You can travel:', ) 
